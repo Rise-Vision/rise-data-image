5b30df5ecc6dc70682cd3049be0205b953fcadca
santiagonoguez <santiago@nul-unu.com>
2018-09-25 13:40:29 -0500

    add event for manual testing

--- src/rise-data-image.js
+++ src/rise-data-image.js
@@ -1,11 +1,34 @@
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+/* eslint-disable no-magic-numbers */
+
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
 class RiseDataImage extends PolymerElement {
-    static get template() {
-      return html`
-        <p>Image test</p>
-      `;
-    }
+  static get properties () {
+    return {
+      url: {
+        type: String,
+        value: ''
+      }
+    };
+  }
+
+  constructor() {
+    super();
+
+    setInterval(() => this.toggleImage(this), 5000);
+  }
+
+  toggleImage(element) {
+    element.url = `/content/${
+      element.url.endsWith('logo.svg') ? 'schedules-icon.png' : 'logo.svg'
+    }`;
+
+    const event = new CustomEvent('url-updated', {
+      bubbles: true, composed: true, detail: { url: element.url }
+    });
+
+    this.dispatchEvent(event);
+  }
 }
 
 customElements.define('rise-data-image', RiseDataImage);
