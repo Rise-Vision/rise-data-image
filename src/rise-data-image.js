7498ae12df4b7f766827f4abe4679d73aa491303
santiagonoguez <santiago@nul-unu.com>
2018-09-25 09:58:03 -0500

    add content text to facilitate manual testing

--- src/rise-data-image.js
+++ src/rise-data-image.js
@@ -1,6 +1,11 @@
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 
 class RiseDataImage extends PolymerElement {
+    static get template() {
+      return html`
+        <p>Downloading image, please wait...</p>
+      `;
+    }
 }
 
 customElements.define('rise-data-image', RiseDataImage);
