d6575058201ca7cd9562f9a18fb422687d73acf6
santiagonoguez <santiago@nul-unu.com>
2018-09-27 09:16:16 -0500

    add file attribute

--- src/rise-data-image.js
+++ src/rise-data-image.js
@@ -5,6 +5,10 @@ import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 class RiseDataImage extends PolymerElement {
   static get properties () {
     return {
+      file: {
+        type: String,
+        value: ''
+      },
       url: {
         type: String,
         value: ''
@@ -15,12 +19,14 @@ class RiseDataImage extends PolymerElement {
   constructor() {
     super();
 
+    this.file = this.getAttribute('file');
+
     setTimeout(() => this.loadImage(this), 5000);
   }
 
   loadImage(element) {
     // fixed for now, URL will be read from local-storage in a later POC
-    element.url = '/content/logo.svg';
+    element.url = element.file;
 
     const event = new CustomEvent('url-updated', {
       bubbles: true, composed: true, detail: { url: element.url }
