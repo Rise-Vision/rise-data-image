83732883493fd371138b467b8a64883246dae847
santiagonoguez <santiago@nul-unu.com>
2018-09-28 11:11:47 -0500

    implement single file watch

--- src/rise-data-image.js
+++ src/rise-data-image.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-magic-numbers */
+/* eslint-disable no-magic-numbers, no-warning-comments */
 
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
@@ -21,20 +21,22 @@ class RiseDataImage extends PolymerElement {
 
     this.file = this.getAttribute('file');
 
-    this.watch(this.file, message => this.handleUpdate(message));
+    // TODO: check license ( JTBD later on this epic )
+
+    watchSingleFile(this.file, message => this.handleUpdate(message));
   }
 
   handleUpdate(message) {
-    if (!message.fileUrl) {
+    if (!message.available) {
       // file doesn't exist or deleted, handle this
 
       return;
     }
 
-    this.loadImage(message.fileUrl);
+    this.sendImageUrlUpdatedEvent(message.fileUrl);
   }
 
-  loadImage(url) {
+  sendImageUrlUpdatedEvent(url) {
     this.url = url;
 
     const event = new CustomEvent('url-updated', {
@@ -44,11 +46,66 @@ class RiseDataImage extends PolymerElement {
     this.dispatchEvent(event);
   }
 
-  // this will be sent to common-template later
-  watch(filePath, handler) {
-    setTimeout(() => handler({ filePath, fileUrl: this.file }), 5000);
+}
+
+// Move to common-templates
+
+function watchSingleFile(filePath, handler) {
+  onceAvailable('local-storage', () =>
+  {
+    const state = {filePath, available: false, error: false};
+
+    RisePlayerConfiguration.LocalMessaging.broadcastMessage({
+      topic: "watch", filePath
+    });
+
+    RisePlayerConfiguration.LocalMessaging.receiveMessages(message => {
+      if (!message || !message.topic) {return;}
+
+      switch (message.topic.toUpperCase()) {
+        case "FILE-UPDATE":
+          return handleFileUpdate(message, state, handler);
+        case "FILE-ERROR":
+          return handleFileError(message, state, handler);
+      }
+    });
+  });
+}
+
+function onceAvailable(modules, action) {
+  // TODO: check if modules available, then activate action.
+
+  action();
+}
+
+function handleFileUpdate(message, state, handler) {
+  if (!message || !message.filePath || !message.status || message.filePath !== state.filePath) {
+    return;
   }
 
+  const available = message.status.toUpperCase() === "CURRENT";
+
+  // availability hasn't changed, so don't handle
+  if (available === state.available) {return;}
+
+  Object.assign(state, {available, error: false});
+
+  const fileUrl = available ? message.osurl : null;
+
+  handler({ fileUrl, available });
+}
+
+function handleFileError(message, state, handler) {
+  if (!message || !message.filePath) {return;}
+
+  // file is not being watched
+  if (message.filePath !== state.filePath) {return;}
+
+  Object.assign(state, {available, error: true});
+
+  handler({
+    available, error, errorMessage: message.msg, errorDetail: message.detail
+  });
 }
 
 customElements.define('rise-data-image', RiseDataImage);
