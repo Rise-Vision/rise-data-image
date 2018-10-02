f3a14b9df0e360df31422b0ce41509f43a080e74
santiagonoguez <santiago@nul-unu.com>
2018-10-02 09:08:18 -0500

    watch file and add unit test for happy path

--- src/rise-data-image.js
+++ src/rise-data-image.js
@@ -23,7 +23,9 @@ class RiseDataImage extends PolymerElement {
 
     // TODO: check license ( JTBD later on this epic )
 
-    watchSingleFile(this.file, message => this.handleUpdate(message));
+    RisePlayerConfiguration.LocalStorage.watchSingleFile(
+      this.file, message => this.handleUpdate(message)
+    );
   }
 
   handleUpdate(message) {
@@ -46,66 +48,6 @@ class RiseDataImage extends PolymerElement {
     this.dispatchEvent(event);
   }
 
-}
-
-// Move to common-templates
-
-function watchSingleFile(filePath, handler) {
-  onceAvailable('local-storage', () =>
-  {
-    const state = {filePath, available: false, error: false};
-
-    RisePlayerConfiguration.LocalMessaging.broadcastMessage({
-      topic: "watch", filePath
-    });
-
-    RisePlayerConfiguration.LocalMessaging.receiveMessages(message => {
-      if (!message || !message.topic) {return;}
-
-      switch (message.topic.toUpperCase()) {
-        case "FILE-UPDATE":
-          return handleFileUpdate(message, state, handler);
-        case "FILE-ERROR":
-          return handleFileError(message, state, handler);
-      }
-    });
-  });
-}
-
-function onceAvailable(modules, action) {
-  // TODO: check if modules available, then activate action.
-
-  action();
-}
-
-function handleFileUpdate(message, state, handler) {
-  if (!message || !message.filePath || !message.status || message.filePath !== state.filePath) {
-    return;
-  }
-
-  const available = message.status.toUpperCase() === "CURRENT";
-
-  // availability hasn't changed, so don't handle
-  if (available === state.available) {return;}
-
-  Object.assign(state, {available, error: false});
-
-  const fileUrl = available ? message.osurl : null;
-
-  handler({ fileUrl, available });
-}
-
-function handleFileError(message, state, handler) {
-  if (!message || !message.filePath) {return;}
-
-  // file is not being watched
-  if (message.filePath !== state.filePath) {return;}
-
-  Object.assign(state, {available, error: true});
-
-  handler({
-    available, error, errorMessage: message.msg, errorDetail: message.detail
-  });
 }
 
 customElements.define('rise-data-image', RiseDataImage);
