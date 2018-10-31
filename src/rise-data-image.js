1a5dc853eb61720150e3af4b803f502a799dd063
Stuart Lees <stuartlees@gmail.com>
2018-10-31 15:49:03 -0400

    Revise eslint config to be consistent across other component repos

--- src/rise-data-image.js
+++ src/rise-data-image.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-warning-comments */
 
-import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { PolymerElement } from "@polymer/polymer/polymer-element.js";
 import { version } from "./rise-data-image-version.js";
 
 class RiseDataImage extends PolymerElement {
-  static get properties () {
+  static get properties() {
     return {
       file: {
         type: String,
-        value: ''
+        value: ""
       },
       url: {
         type: String,
-        value: ''
+        value: ""
       }
     };
   }
@@ -37,7 +37,7 @@ class RiseDataImage extends PolymerElement {
   constructor() {
     super();
 
-    this.file = this.getAttribute('file');
+    this.file = this.getAttribute( "file" );
   }
 
   ready() {
@@ -48,46 +48,46 @@ class RiseDataImage extends PolymerElement {
       () => this._handleStart(),
       { once: true }
     );
-    this._sendImageEvent(RiseDataImage.EVENT_CONFIGURED);
+    this._sendImageEvent( RiseDataImage.EVENT_CONFIGURED );
   }
 
   _handleStart() {
     // TODO: check license ( JTBD later on this epic )
 
     RisePlayerConfiguration.LocalStorage.watchSingleFile(
-      this.file, message => this._handleSingleFileUpdate(message)
+      this.file, message => this._handleSingleFileUpdate( message )
     );
-    console.log("version is: ", version); // eslint-disable-line no-console
+    console.log( "version is: ", version ); // eslint-disable-line no-console
   }
 
-  _handleSingleFileUpdate(message) {
-    if (!message.status) {
+  _handleSingleFileUpdate( message ) {
+    if ( !message.status ) {
       return;
     }
 
-    this.url = message.fileUrl || '';
+    this.url = message.fileUrl || "";
 
-    if (message.status === 'FILE-ERROR') {
-      return this._sendImageEvent(RiseDataImage.EVENT_IMAGE_ERROR, {
+    if ( message.status === "FILE-ERROR" ) {
+      return this._sendImageEvent( RiseDataImage.EVENT_IMAGE_ERROR, {
         file: this.file,
         errorMessage: message.errorMessage,
         errorDetail: message.errorDetail
       });
     }
 
-    this._sendImageEvent(RiseDataImage.EVENT_IMAGE_STATUS_UPDATED, {
+    this._sendImageEvent( RiseDataImage.EVENT_IMAGE_STATUS_UPDATED, {
       file: this.file, url: this.url, status: message.status
     });
   }
 
-  _sendImageEvent(eventName, detail = {}) {
-    const event = new CustomEvent(eventName, {
+  _sendImageEvent( eventName, detail = {}) {
+    const event = new CustomEvent( eventName, {
       bubbles: true, composed: true, detail
     });
 
-    this.dispatchEvent(event);
+    this.dispatchEvent( event );
   }
 
 }
 
-customElements.define('rise-data-image', RiseDataImage);
+customElements.define( "rise-data-image", RiseDataImage );
